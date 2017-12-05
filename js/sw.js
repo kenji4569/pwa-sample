41a23807a4e94aedd6511fe7af6ec9c1e4173d4b
Kenji Hosoda <hosoda@s-cubism.jp>
2017-12-05 20:20:56 +0900

    Revert sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,44 +1,53 @@
-importScripts('https://www.gstatic.com/firebasejs/4.7.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/4.7.0/firebase-messaging.js');
-importScripts('./init.js');
 
-const messaging = firebase.messaging();
-
-/**
- * Here is is the code snippet to initialize Firebase Messaging in the Service
- * Worker when your app is not hosted on Firebase Hosting.
- // [START initialize_firebase_in_sw]
- // Give the service worker access to Firebase Messaging.
- // Note that you can only use Firebase Messaging here, other Firebase libraries
- // are not available in the service worker.
- importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
- importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
- // Initialize the Firebase app in the service worker by passing in the
- // messagingSenderId.
- firebase.initializeApp({
-   'messagingSenderId': 'YOUR-SENDER-ID'
- });
- // Retrieve an instance of Firebase Messaging so that it can handle background
- // messages.
- const messaging = firebase.messaging();
- // [END initialize_firebase_in_sw]
- **/
-
-
-// If you would like to customize notifications that are received in the
-// background (Web app is closed or not in browser focus) then you should
-// implement this optional method.
-// [START background_handler]
-messaging.setBackgroundMessageHandler(function(payload) {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  // Customize notification here
-  const notificationTitle = 'Background Message Title';
-  const notificationOptions = {
-    body: 'Background Message body.',
-    icon: '/firebase-logo.png'
-  };
-
-  return self.registration.showNotification(notificationTitle,
-      notificationOptions);
+//ServiceWorkerにインストールされるスクリプト
+//プッシュ通知が行われると「push」イベントが起動する
+self.addEventListener("install", function(event) {
+    self.skipWaiting();
+    console.log("Installed", event);
 });
-// [END background_handler]
+
+self.addEventListener("activate", function(event) {
+    console.log("Activated", event);
+});
+
+self.addEventListener("push", function(event) {
+    console.log("Push message received", event);
+    self.registration.pushManager.getSubscription().then(function(subscription) {
+        if (subscription) {
+            console.log("Data:", event.data);
+            self.registration.showNotification('title', {
+              body: 'xxx',
+              tag: "push-test",
+              actions: [{
+                  action: "act1",
+                  title: "ボタン１"
+              }, {
+                  action: "act2",
+                  title: "ボタン２"
+              }]
+          })
+        } else {
+          throw new Error("User not subscribed");
+        }
+    });
+});
+//押したaction名はnotificationclickのevent.actionで取得できます。
+
+self.addEventListener("notificationclick", function(event) {
+    console.log("notification clicked:" + event);
+    console.log("action:" + event.action);
+    event.notification.close();
+
+    var url = "/";
+    if (event.notification.data.url) {
+        url = event.notification.data.url
+    }
+
+    event.waitUntil(
+            clients.matchAll({type: "window"}).then(function() {
+            if(clients.openWindow) {
+              return clients.openWindow(url)
+            }
+        })
+    );
+});
