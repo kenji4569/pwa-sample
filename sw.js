181fd2103812fa9bab955a6670ab918971eee2b2
Kenji Hosoda <hosoda@s-cubism.jp>
2017-12-05 21:00:52 +0900

    Use firebase message for notification

--- sw.js
+++ sw.js
@@ -1,1 +1,19 @@
-importScripts("./js/sw.js");
+// importScripts("./js/sw.js");
+//
+importScripts('https://www.gstatic.com/firebasejs/4.7.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.7.0/firebase-messaging.js');
+importScripts('./init.js');
+
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    body: 'Background Message body.'
+  };
+
+  return self.registration.showNotification(notificationTitle,
+      notificationOptions);
+});
